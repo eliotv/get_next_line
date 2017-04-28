3fc88590fe0cf60a444726819f94b7f0903f261b
Eliot vanHeumen <Eliot@Eliots-MacBook-Air.local>
2017-04-28 13:16:02 -0700

    get

--- get_next_line.c
+++ get_next_line.c
@@ -5,57 +5,70 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: evanheum <evanheum@student.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/17 16:40:52 by evanheum          #+#    #+#             */
-/*   Updated: 2017/04/18 21:10:09 by evanheum         ###   ########.fr       */
+/*   Created: 2017/04/19 18:55:30 by evanheum          #+#    #+#             */
+/*   Updated: 2017/04/24 18:44:14 by evanheum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int read_file_bytes(int const fd, char **file)
+void fill_line(char **file_str, char *buff)
 {
-	static char buff[BUFF_SIZE + 1];
-	int	byte_count;
-	char *nstr;
+	char *tmp;
 
-	byte_count = read(fd, buff, BUFF_SIZE);
-	if (byte_count > 0)
+	if (*file_str == 0)
 	{
-		nstr = ft_strjoin(*file, buff);
-		if (!nstr)
-			return (-1);
-		free (*file);
-		*file = nstr;
+		*file_str = ft_strdup(buff);
+		ft_bzero(buff, BUFF_SIZE + 1);
+		return ;
 	}
-	return (byte_count);
+	tmp = ft_strjoin(*file_str, buff);
+	free(*file_str);
+	ft_bzero(buff, BUFF_SIZE + 1);
+	*file_str = tmp;
+}
+
+int merge_line(char **file_str, char **line, size_t len)
+{
+	*line = ft_strndup(*file_str, len);
+	*file_str = ft_memmove(*file_str, *file_str + len + 1, ft_strlen(*file_str));
+		return (1);
+}
+
+int end_line(char **file_str, char **line)
+{
+	size_t len;
+
+	len = ft_strlen(*file_str);
+	*line = ft_strdup(*file_str);
+	ft_bzero(*file_str, len);
+	return (1);
 }
 
 int get_next_line(int const fd, char **line)
 {
-	static char *file;
-	char *newline_index;
-	int  ret;
+	static char *file_str[4864];
+	char buff[BUFF_SIZE + 1];
+	char *tmp;
+	int bytes_read;
 
-	if (!file && (file = (char *)ft_memalloc(sizeof(char))) == NULL)
+	if  (fd < 0 || BUFF_SIZE < 1 || !line)
 		return (-1);
-	newline_index = ft_strchr(file, NL);
-	while (newline_index == NULL)
+	ft_bzero(buff, BUFF_SIZE + 1);
+	while ((bytes_read = read(fd, buff, BUFF_SIZE)))
 	{
-		ret = read_file_bytes(fd, &file);
-		if (ret == 0)
-		{
-			if ((newline_index = ft_strchr(file, '\0')) == file)
-				return (0);
-		}
-		else if (ret < 0)
-			return (-1);
-		else
-			newline_index = ft_strchr(file, NL);
+		fill_line(&file_str[fd], buff);
+		if ((tmp = ft_strchr(file_str[fd], '\n')) != 0)
+			return (merge_line(&file_str[fd], line, tmp - file_str[fd]));
 	}
-	*line = ft_strsub(file, 0, NL);
-	if (!*line)
-		return (-1);
-	newline_index = ft_strdup(newline_index + 1);
-	free(file);
-	file = newline_index;
-	return (1);
+	if (!file_str[fd])
+		return (0);
+	else if ((tmp = ft_strchr(file_str[fd], '\n')) != 0)
+		return (merge_line(&file_str[fd], line, tmp - file_str[fd]));
+	else if (ft_strlen(file_str[fd]) > 0)
+		return (end_line(&file_str[fd], line));
+	free(&file_str[fd]);
+	return (0);
 }
